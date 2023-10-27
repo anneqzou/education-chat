d3fa080b4dc7a42b3cb930706488fcd4ffaf63c7
Qiong Zou <anne.q.zou@gmail.com>
2023-10-27 14:24:59 +0800

    Add build in prompt.

---
+++ buildin_prompt.py
@@ -0,0 +1,18 @@
+buildInPrompts = [
+      {
+        "title": "Generate Math learning roadmap for Grade 7",
+        "prompt": "Introduce all areas or topics to study for Grade 7 Math (following Common Core math content standards). First, please list standards, second, structure the skills with Mermaid diagram definition (avoid square bracket or round bracket in definition)."
+      },
+      {
+        "title": "Generate Language Art learning roadmap for Grade 7",
+        "prompt": "Introduce all areas or topics to study for Grade 7 Language Art (following Common Core Language Art content standards). First, please list standards, second, structure the skills with Mermaid diagram definition (avoid square bracket or round bracket in definition)."
+      },
+      {
+        "title": "Grammar and mechanics: Is the sentence declarative, interrogative, imperative, or exclamatory?",
+        "prompt": "Practice skill for Grammar and mechanics which is Is the sentence declarative, interrogative, imperative, or exclamatory. Here is example: I had no idea you played the piano so beautifully! Determine the type of this sentence: 1) interrogative; 2) declarative; 3) exclamatory. First pls explain the skills required to solve the question, second structure the skills with Mermaid diagram definition (avoid square bracket or round bracket in definition). Finally provide questions for user to practice, when you do so, pls generate them with different level and put them in a table."
+      }, 
+      {
+        "title": "Math: Find missing angles in special triangles",
+        "prompt": "Practice skill for Math which is Find missing angles in special triangles. Here is example: A right triangle has one angle that measures 30 degree. What is the measure of the other acute angle. First pls explain the skills required to solve the question, second structure the skills with Mermaid diagram definition (avoid square bracket or round bracket in definition). Finally provide questions for user to practice, when you do so, pls generate them with different level and put them in a table."
+      }
+ ]
--- education_chat.py
+++ education_chat.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from AzureGPT_API import CallAzureGPT,CallAzure_GPT35
+from AzureGPT_API import CallAzureGPT, CallAzure_GPT35
 from helper import new_chat, generate_conversion
+from buildin_prompt import buildInPrompts
 
 # Initialize session_state
 if "messages" not in st.session_state:
@@ -11,15 +12,50 @@ if "stored_session" not in st.session_state:
 
 if "first_message_in_sesson" not in st.session_state:
     st.session_state["first_message_in_sesson"] = False
+
+if "trigger_by_built_in_prompt" not in st.session_state:
+    st.session_state["trigger_by_built_in_prompt"] = False
+
 # End of Initialize session_state
 
 # Set up sidebar with various options
-with st.sidebar.expander("🛠️ ", expanded=False):
+with st.sidebar:
     # Option for model
-    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','gpt-4'], index=0)
+    MODEL = st.radio(
+        "Select Model You Want To Use",
+        ["gpt-4", "gpt-3.5-turbo"]
+    )
+    if MODEL == 'gpt-3.5-turbo':
+        st.write('You selected gpt-3.5-turbo.')
+    else:
+        st.write("You selected gpt-4")
 
 st.title("Education Chatbot")
 st.subheader(" Powered by 🦜 Azure GPT + Streamlit")
+
+def send_button_ques(prompt):
+    with st.container():
+        # Display user message in chat message container
+        st.session_state["trigger_by_built_in_prompt"] = True
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        if MODEL == "gpt-4":
+            response = CallAzureGPT(prompt)
+        elif MODEL == "gpt-3.5-turbo":
+            response = CallAzure_GPT35(prompt)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        if st.session_state["first_message_in_sesson"] == False:
+            st.session_state["first_message_in_sesson"] = True
+
+# Render suggested question buttons
+buttons = st.container()
+for q in buildInPrompts:
+    button_ques = buttons.button(
+        label=q["title"], 
+        on_click=send_button_ques, 
+        args=[q["prompt"]] 
+    )
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -29,8 +65,8 @@ for message in st.session_state.messages:
 with st.container():
     # React to user input
     if prompt := st.chat_input("What is up?"):
+        st.session_state["trigger_by_built_in_prompt"] = False
         # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -40,19 +76,17 @@ with st.container():
             response = CallAzure_GPT35(prompt)
 
         # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            st.session_state["first_message_in_sesson"] = True
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state["first_message_in_sesson"] = True
 
 if st.session_state["first_message_in_sesson"] == True:
     st.download_button(label="Download", data=generate_conversion(), key="assistant-download")
 
 # Add a button to start a new chat
-st.sidebar.button("New Chat", on_click = new_chat, type='primary')
+st.sidebar.button("New Chat", on_click=new_chat, type='primary')
 
 # Display stored conversation sessions in the sidebar
 for i, sublist in enumerate(st.session_state.stored_session):
-    with st.sidebar.expander(label= f"Session:{i}"):
+    with st.sidebar.expander(label=f"Session:{i}"):
         st.write(sublist)
